47aa9e31d6e5e4724dd5d32f1530422cdaf6e4a8
clementemoreira <cmoreira@e-goi.com>
2021-11-12 10:50:55 +0000

    Varias cenas7

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
 var id = null;
+var back = false;
+var pause = true;
 function myMove() {
-  var elem = document.getElementById("myAnimation");
-  var pos = 0;
-  clearInterval(id);
-  id = setInterval(frame, 10);
-  function frame() {
-    if (pos == 350) {
-      clearInterval(id);
-    } else {
-      pos++;
-      elem.style.top = pos + 'px';
-      elem.style.left = pos + 'px';
+  if(pause==true){
+    pause=false;
+  }else{
+    pause=true;
+  }
+  if(!pause){
+    var elem = document.getElementById("myAnimation");
+    var pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 10);
+    function frame() {
+      if (pos == 350) {
+        back = true;
+      }
+      if (pos == 0){
+        back = false;
+      }
+  
+      if(back){
+        pos--;
+        elem.style.top = pos + 'px';
+        elem.style.left = pos + 'px';
+      }else{
+        pos++;
+        elem.style.top = pos + 'px';
+        elem.style.left = pos + 'px';
+      }
     }
   }
-}
-
-while(true){
-  myMove();
+ 
 }
